11a3af168afd5b1e25ddb02f0561cebacbb38682
Yasitha <yasithanadeeshan@gmail.com>
2022-01-28 21:31:07 +0530

    connect mysql database

--- backend/app.py
+++ backend/app.py
@@ -5,5 +5,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
+
+# confi the mysql data base
+# mysql://username:password@server/db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/electronjs_flask_ex'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 if __name__ == '__main__':
     app.run(debug=True)
