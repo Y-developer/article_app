24452580372d7fe6583c7cb27f3a9d31a82ff1f2
Yasitha <yasithanadeeshan@gmail.com>
2022-01-28 21:34:31 +0530

    create APIs for  get_articles()  get_details(id)  add_article()  update_article(id)  delete_article(id)

--- backend/app.py
+++ backend/app.py
@@ -33,5 +33,45 @@ class ArticlesSchema(ma.Schema):
 
 article_schema = ArticlesSchema()
 articles_schema = ArticlesSchema(many=True)
+
+
+# APIs
+@app.route('/get',methods=['GET'])
+def get_articles():
+    all_articles = Articles.query.all()
+    results = articles_schema.dump(all_articles)
+    return jsonify(results)
+
+@app.route('/get/<id>/',methods=['GET'])
+def get_details(id):
+    article = Articles.query.get(id)
+    return article_schema.jsonify(article)
+
+@app.route('/add',methods=['POST'])
+def add_article():
+    title = request.json['title']
+    body = request.json['body']
+    articles = Articles(title,body)
+    db.session.add(articles)
+    db.session.commit()
+    return article_schema.jsonify(articles)
+
+@app.route('/update/<id>/',methods=['PUT'])
+def update_article(id):
+    article = Articles.query.get(id)
+    title = request.json['title']
+    body = request.json['body']
+    article.title = title
+    article.body = body
+    db.session.commit()
+    return article_schema.jsonify(article)
+
+@app.route('/delete/<id>/',methods=['DELETE'])
+def delete_article(id):
+    article = Articles.query.get(id)
+    db.session.delete(article)
+    db.session.commit()
+    return article_schema.jsonify(article)
+
 if __name__ == '__main__':
     app.run(debug=True)
