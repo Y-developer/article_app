dae96f4efb57e383fecc4d5399b27700f6e630a8
Yasitha <yasithanadeeshan@gmail.com>
2022-01-28 21:30:35 +0530

    init app

--- backend/app.py
+++ backend/app.py
@@ -3,3 +3,7 @@ import datetime
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+
+app = Flask(__name__)
+if __name__ == '__main__':
+    app.run(debug=True)
